bccaca31d200856af3944b073171e5dfbc014bfd
Zubayr Khalid <write2zubayr@gmail.com>
2025-04-19 17:32:41 +0200

    complete make

--- test_program/src/instructions/make.rs
+++ test_program/src/instructions/make.rs
@@ -2,6 +2,7 @@ use pinocchio::account_info::AccountInfo;
 use pinocchio::program_error::ProgramError;
 use pinocchio::program_result::ProgramResult;
 use pinocchio_system::instructions::CreateAccount;
+use pinocchio_token::pubkey;
 
 pub fn process_make_instruction(
     accounts_info: &[AccountInfo],
@@ -18,10 +19,27 @@ pub fn process_make_instruction(
         receive_account,
         token_program_account,
         system_program_account,
+        _remaining @..
 
     ] = &accounts_info[..] else {
         return Err(ProgramError::InvalidAccountData);
     };
+
+    let bump = unsafe{ *(instruction_data.as_ptr() as *const u8) }.to_le_bytes();
+    let seed = [b"escrow", maker.key().as_slice(), bump.as_ref()];
+    let signer_seeds = &seed[..];
+
+    let pda = pubkey::checked_create_program_address(&seed, &crate::ID).unwrap();
+
+    assert_eq!(&pda, escrow_account.key());
+
+    unsafe{
+
+        assert_eq!(mint_a_account.owner(), &pinocchio_token::ID);
+        assert_eq!(mint_b_account.owner(), &pinocchio_token::ID);
+
+
+    }
 
     CreateAccount{
         from: maker_account.key(),
@@ -31,6 +49,20 @@ pub fn process_make_instruction(
         owner: crate::ID,
     }.invoke()?;
 
+    let escrow_account = Esrow::from_account_info_unchecked(&escrow_account);
+
+    escrow_account.maker = *maker_account.key();
+    escrow_account.mint_a_account = *mint_a_account.key();
+    escrow_account.mint_b_account = *mint_b_account.key();
+    escrow_account.receive_account = *receive_account.key();
+    escrow_account.bump = *data.as_ptr();
+
+    pinocchio_token::instructions::Transfer {
+        from: maker_ata_account,
+        to: vault_account,
+        amount: receive_account.amount,
+        authority: maker_account.key(),
+    }.invoke()?;
 
     Ok(())
 }
