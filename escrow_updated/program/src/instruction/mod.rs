786f477c9101f40b26bafaf5c73dfdd88cb8b782
zubayr1 <write2zubayr@gmail.com>
2025-07-15 19:56:19 +0200

    update instruction mod

--- escrow_updated/program/src/instruction/mod.rs
+++ escrow_updated/program/src/instruction/mod.rs
@@ -1,41 +1,56 @@
 use pinocchio::program_error::ProgramError;
 
-pub mod initialize_mystate;
-pub mod update_mystate;
+pub mod make;
+pub mod take;
+pub mod create_account_checked;
 
-pub use initialize_mystate::*;
-pub use update_mystate::*;
+pub use make::*;
+pub use take::*;
+pub use create_account_checked::*;
 
 #[repr(u8)]
-pub enum MyProgramInstruction {
-    InitializeState,
-    UpdateState,
+pub enum EscrowInstruction {
+    MakeEscrow,
+    TakeEscrow,
 }
 
-impl TryFrom<&u8> for MyProgramInstruction {
+impl TryFrom<&u8> for EscrowInstruction {
     type Error = ProgramError;
 
     fn try_from(value: &u8) -> Result<Self, Self::Error> {
         match *value {
-            0 => Ok(MyProgramInstruction::InitializeState),
-            1 => Ok(MyProgramInstruction::UpdateState),
+            0 => Ok(EscrowInstruction::MakeEscrow),
+            1 => Ok(EscrowInstruction::TakeEscrow),
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
 }
 
 mod idl_gen {
-    use super::InitializeMyStateIxData;
+    use super::{MakeEscrowIxData, TakeEscrowIxData};
 
     #[derive(shank::ShankInstruction)]
-    enum _MyProgramInstruction {
-        #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
-        #[account(1, writable, name = "state_acc", desc = "New State account")]
-        #[account(2, name = "sysvar_rent_acc", desc = "Sysvar rent account")]
-        #[account(3, name = "system_program_acc", desc = "System program account")]
-        InitializeState(InitializeMyStateIxData),
-        #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
-        #[account(1, writable, name = "state_acc", desc = "State account")]
-        UpdateState,
+    enum _EscrowInstruction {
+        #[account(0, writable, signer, name = "maker", desc = "The user creating the escrow")]
+        #[account(1, name = "mint_a", desc = "The mint of the token the maker is offering")]
+        #[account(2, writable, name = "escrow_acc", desc = "The escrow state account (PDA)")]
+        #[account(3, writable, name = "vault", desc = "The vault token account (PDA)")]
+        #[account(4, writable, name = "maker_ata_a", desc = "The maker's token account for mint A")]
+        #[account(5, name = "system_program", desc = "System program")]
+        #[account(6, name = "token_program", desc = "Token program")]
+        #[account(7, name = "rent", desc = "Rent sysvar")]
+        MakeEscrow(MakeEscrowIxData),
+
+        #[account(0, writable, signer, name = "taker", desc = "The user fulfilling the escrow")]
+        #[account(1, writable, name = "maker", desc = "The original maker of the escrow")]
+        #[account(2, writable, name = "escrow_acc", desc = "The escrow state account to be closed")]
+        #[account(3, writable, name = "vault", desc = "The vault account to be closed")]
+        #[account(4, name = "mint_a", desc = "The mint of the token the maker offered")]
+        #[account(5, name = "mint_b", desc = "The mint of the token the taker is offering")]
+        #[account(6, writable, name = "taker_ata_a", desc = "The taker's token account for mint A")]
+        #[account(7, writable, name = "taker_ata_b", desc = "The taker's token account for mint B")]
+        #[account(8, writable, name = "maker_ata_b", desc = "The maker's token account for mint B")]
+        #[account(9, name = "system_program", desc = "System program")]
+        TakeEscrow(TakeEscrowIxData),
     }
 }
