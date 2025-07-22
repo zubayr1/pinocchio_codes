857dda83376d4983efc2aa10c26058fccdccb315
zubayr1 <write2zubayr@gmail.com>
2025-07-22 21:09:52 +0200

    update mod imports

--- pinocchio-multisig/program/src/instruction/mod.rs
+++ pinocchio-multisig/program/src/instruction/mod.rs
@@ -1,41 +1,47 @@
 use pinocchio::program_error::ProgramError;
 
-pub mod initialize_mystate;
-pub mod update_mystate;
+// pub mod multisig_create;
+pub mod multisig_config;
+pub mod multisig_create;
+pub mod update_members;
 
-pub use initialize_mystate::*;
-pub use update_mystate::*;
+// pub use multisig_create::*;
+pub use multisig_config::*;
+pub use multisig_create::*;
+pub use update_members::*;
 
 #[repr(u8)]
-pub enum MyProgramInstruction {
-    InitializeState,
-    UpdateState,
+pub enum MultisigInstruction {
+    InitializeConfig,
+    CreateMultisig,
+    UpdateMembers,
 }
 
-impl TryFrom<&u8> for MyProgramInstruction {
+impl TryFrom<&u8> for MultisigInstruction {
     type Error = ProgramError;
 
     fn try_from(value: &u8) -> Result<Self, Self::Error> {
         match *value {
-            0 => Ok(MyProgramInstruction::InitializeState),
-            1 => Ok(MyProgramInstruction::UpdateState),
+            0 => Ok(MultisigInstruction::InitializeConfig),
+            1 => Ok(MultisigInstruction::CreateMultisig),
+            2 => Ok(MultisigInstruction::UpdateMembers),
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
 }
 
-mod idl_gen {
-    use super::InitializeMyStateV1IxData;
+// mod idl_gen {
+//     use super::InitializeMyStateV1IxData;
 
-    #[derive(shank::ShankInstruction)]
-    enum _MyProgramInstruction {
-        #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
-        #[account(1, writable, name = "state_acc", desc = "New State account")]
-        #[account(2, name = "sysvar_rent_acc", desc = "Sysvar rent account")]
-        #[account(3, name = "system_program_acc", desc = "System program account")]
-        InitializeState(InitializeMyStateV1IxData),
-        #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
-        #[account(1, writable, name = "state_acc", desc = "State account")]
-        UpdateState,
-    }
-}
+//     #[derive(shank::ShankInstruction)]
+//     enum _MyProgramInstruction {
+//         #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
+//         #[account(1, writable, name = "state_acc", desc = "New State account")]
+//         #[account(2, name = "sysvar_rent_acc", desc = "Sysvar rent account")]
+//         #[account(3, name = "system_program_acc", desc = "System program account")]
+//         InitializeState(InitializeMyStateV1IxData),
+//         #[account(0, writable, signer, name = "payer_acc", desc = "Fee payer account")]
+//         #[account(1, writable, name = "state_acc", desc = "State account")]
+//         UpdateState,
+//     }
+// }
--- pinocchio-multisig/program/src/state/mod.rs
+++ pinocchio-multisig/program/src/state/mod.rs
@@ -1,5 +1,9 @@
-pub mod my_state;
+pub mod multisig_state;
+pub mod multisig_config;
+pub mod member_state;
 pub mod utils;
 
-pub use my_state::*;
+pub use multisig_state::*;
+pub use multisig_config::*;
+pub use member_state::*;
 pub use utils::*;
