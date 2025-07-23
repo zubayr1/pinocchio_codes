035f345a1ccfb638f44598cca006f8c36d11144c
zubayr1 <write2zubayr@gmail.com>
2025-07-23 18:04:42 +0200

    update state mod

--- pinocchio-multisig/program/src/state/mod.rs
+++ pinocchio-multisig/program/src/state/mod.rs
@@ -1,9 +1,14 @@
 pub mod multisig_state;
 pub mod multisig_config;
 pub mod member_state;
+pub mod transaction;
+pub mod proposal;
 pub mod utils;
 
 pub use multisig_state::*;
 pub use multisig_config::*;
 pub use member_state::*;
+pub use transaction::*;
+pub use proposal::*;
 pub use utils::*;
+
