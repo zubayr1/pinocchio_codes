1d82bccbd05700c4f95c809e086713e7478141d5
zubayr1 <write2zubayr@gmail.com>
2025-07-23 18:06:12 +0200

    update instruction mods

--- pinocchio-multisig/program/src/instruction/mod.rs
+++ pinocchio-multisig/program/src/instruction/mod.rs
@@ -4,17 +4,38 @@ use pinocchio::program_error::ProgramError;
 pub mod multisig_config;
 pub mod multisig_create;
 pub mod update_members;
+pub mod create_proposal;
+pub mod create_transaction;
+pub mod vote;
+pub mod update_proposal_result;
+pub mod approve_transaction;
+pub mod execute_transaction;
+pub mod stale_transaction_index;
 
 // pub use multisig_create::*;
 pub use multisig_config::*;
 pub use multisig_create::*;
 pub use update_members::*;
+pub use create_proposal::*;
+pub use create_transaction::*;
+pub use vote::*;
+pub use update_proposal_result::*;
+pub use approve_transaction::*;
+pub use execute_transaction::*;
+pub use stale_transaction_index::*;
 
 #[repr(u8)]
 pub enum MultisigInstruction {
     InitializeConfig,
     CreateMultisig,
     UpdateMembers,
+    CreateProposal,
+    CreateTransaction,
+    Vote,
+    UpdateProposalResult,
+    ApproveTransaction,
+    ExecuteTransaction,
+    StaleTransactionIndex,
 }
 
 impl TryFrom<&u8> for MultisigInstruction {
@@ -25,6 +46,13 @@ impl TryFrom<&u8> for MultisigInstruction {
             0 => Ok(MultisigInstruction::InitializeConfig),
             1 => Ok(MultisigInstruction::CreateMultisig),
             2 => Ok(MultisigInstruction::UpdateMembers),
+            3 => Ok(MultisigInstruction::CreateTransaction),
+            4 => Ok(MultisigInstruction::CreateProposal),            
+            5 => Ok(MultisigInstruction::Vote),
+            6 => Ok(MultisigInstruction::UpdateProposalResult),
+            7 => Ok(MultisigInstruction::ApproveTransaction),
+            8 => Ok(MultisigInstruction::ExecuteTransaction),
+            9 => Ok(MultisigInstruction::StaleTransactionIndex),
             _ => Err(ProgramError::InvalidInstructionData),
         }
     }
